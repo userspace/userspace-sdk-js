06b7360b4e29c375126eef6f849e888333c36d60
Sebastian Javier Marchano <sebasjm@gmail.com>
2017-02-14 13:37:02 -0300

    support nodejs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
 import decode from 'jwt-decode';
-import Parse from 'parse';
+
+const isNode = typeof process === 'object' && `${process}` === '[object process]';
+
+const Parse = isNode ? require('parse/node') : require('parse');
+const fetcher = isNode ? require('node-fetch') : fetch;
 
 const base = process.env.USERSPACE_GATEWAY || 'https://gateway.user.space';
 
 const urls = {
-  dashboard: () => `${base}/login/?token=${localStorage.id_token}`,
+  dashboard: token => `${base}/login/?token=${token}`,
 };
 
-function userspace(namespace = 'main') {
-  Parse.initialize('userspace');
+function parse(namespace = 'main') {
+  Parse.initialize(namespace);
   Parse.serverURL = `${base}/${namespace}`;
   Parse.login = (creds) => {
     Parse.credentials = creds;
@@ -20,7 +25,7 @@ function userspace(namespace = 'main') {
         // do nothing
     }
 
-    fetch(`${base}/apps?id=${Parse.session.client}`)
+    fetcher(`${base}/apps?id=${Parse.session.client}`)
       .then((res) => {
         Parse.session.owner = res.body.owner;
       });
@@ -37,7 +42,6 @@ function userspace(namespace = 'main') {
     user: user || Parse.session.owner,
   }).save();
 
-  Parse.login(localStorage.id_token);
   return Parse;
 }
 
@@ -67,10 +71,22 @@ class Token {
   }
 }
 
-function watchLogin() {
+function isTokenStillValid(token) {
+  const decoded = decode(token);
+  if (!decoded.exp) return false;
+  const date = new Date(0);
+  date.setUTCSeconds(decoded.exp);
+  return !(date.valueOf() > (new Date().valueOf() + (offsetSeconds * 1000)));
+}
+
+function watchLogin(session) {
+  const token = (window.location.search.match(/[?&]token=(.*)[#&]?/) || []).pop() || session.id_token;
+  if (!token || !isTokenStillValid(token)) window.location = `https://gateway.user.space/sign/${btoa(window.location.origin + window.location.pathname)}`;
+  session.id_token = token;
+
   const hasToken = window.location.search.match(/[?&]token=(.*)[#&]?/);
   if (hasToken && hasToken.length === 2) {
-    localStorage.id_token = hasToken[1];
+    session.id_token = hasToken[1];
     window.location = window.location.origin + window.location.pathname;
   }
 }
@@ -80,28 +96,28 @@ function signin(app) {
   window.location = `${base}/sign/${appCode}`;
 }
 
-function signout() {
-  localStorage.id_token = '';
+function signout(session) {
+  session.id_token = '';
 }
 
-function localToken() {
-  return new Token(localStorage.id_token);
+function localToken(session) {
+  return new Token(session.id_token);
 }
 
-const withAuth = () => ({
+const withAuth = session => ({
   headers: {
-    Authorization: `Bearer ${localStorage.id_token}`,
+    Authorization: `Bearer ${session.id_token}`,
   },
 });
 
-function topapps() {
-  return fetch(`${base}/topapps`, withAuth()).then(res => res.json());
+function topapps(session) {
+  return fetcher(`${base}/topapps`, withAuth(session)).then(res => res.json());
 }
 
-function size() {
-  return fetch(`${base}/size`, withAuth()).then(res => res.json());
+function size(session) {
+  return fetcher(`${base}/size`, withAuth(session)).then(res => res.json());
 }
 
 export {
-  urls, userspace, watchLogin, signin, signout, localToken, size, topapps,
+  urls, parse, watchLogin, signin, signout, localToken, size, topapps,
 };
